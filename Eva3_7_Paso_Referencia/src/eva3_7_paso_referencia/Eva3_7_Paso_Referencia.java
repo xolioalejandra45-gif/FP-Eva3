45fdea1c2ebb04cea76cbe71485742a1660cd197
Alejandra Xolio <xolioalejandra45@gmail.com>
2025-11-11 16:38:25 -0600

    Eva3_7_Paso_Referencia_25550473

---
+++ Eva3_7_Paso_Referencia/src/eva3_7_paso_referencia/Eva3_7_Paso_Referencia.java
@@ -0,0 +1,29 @@
+package eva3_7_paso_referencia;
+
+import java.util.Scanner;
+
+public class Eva3_7_Paso_Referencia {
+
+    public static void main(String[] args) {
+        String cliente[] = new String[5];
+        double credito[] = new double[5];
+        capturarCLientes(cliente, credito);
+        imprimir(cliente, credito);
+    }
+    public static void capturarCLientes(String[] nombre, double[] credito){
+        Scanner input = new Scanner(System.in);
+        for (int i = 0; i < credito.length; i++) {
+            System.out.println("Nombre del cliente: ");
+            nombre[i] = input.nextLine();
+            System.out.println("Credito del cliente: ");
+            credito[i] = input.nextDouble();
+            input.nextLine();
+        }
+    }
+    public static void imprimir(String[] nombre, double[] credito){
+            for (int i = 0; i < credito.length; i++) {
+                System.out.println("Nombre: " + nombre[i] + ", crédito: $" + credito[i] );
+    }
+    
+  }
+}
