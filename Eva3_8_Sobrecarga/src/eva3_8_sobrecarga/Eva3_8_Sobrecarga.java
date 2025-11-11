3471115592e69f600cdcf30c4fd338c2ed00ecc1
Alejandra Xolio <xolioalejandra45@gmail.com>
2025-11-11 16:51:38 -0600

    Eva3_8_Sobrecarga_25550473

---
+++ Eva3_8_Sobrecarga/src/eva3_8_sobrecarga/Eva3_8_Sobrecarga.java
@@ -0,0 +1,14 @@
+package eva3_8_sobrecarga;
+
+public class Eva3_8_Sobrecarga {
+
+    public static void main(String[] args) {
+        System.out.println("hola");
+        System.out.println(100);
+        System.out.println(100.5);
+        System.out.println(true);
+        System.out.println('c');
+        System.out.println(args);
+    }
+    
+}
